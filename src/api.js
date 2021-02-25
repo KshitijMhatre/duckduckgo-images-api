756c1bd4eb5ef37e1bf46740e9e02dbbea6d4174
Francisco Presencia <franciscop@users.noreply.github.com>
2021-02-25 17:00:55 +0900

    Allow it to throw an error like a normal promise

--- src/api.js
+++ src/api.js
@@ -14,67 +14,61 @@ async function image_search({ query, moderate, retries, iterations }) {
 
     let results = [];
 
-    try {
+    let token = await getToken(keywords);
 
-        let token = await getToken(keywords);
-
-        let params = {
-            "l": "wt-wt",
-            "o": "json",
-            "q": keywords,
-            "vqd": token,
-            "f": ",,,",
-            "p": "" + (p)
-        }
-
-        let data = null;
-        let itr = 0;
-
-
-        while (itr < iterations) {
-
-            while (true) {
-                try {
-
-                    let response = await axios.get(reqUrl, {
-                        params,
-                        headers
-                    })
-
-                    data = response.data;
-                    if (!data.results) throw "No results";
-                    break;
-
-                } catch (error) {
-                    console.error(error)
-                    attempt += 1;
-                    if (attempt > retries) {
-                        return new Promise((resolve, reject) => {
-                            resolve(results)
-                        });
-                    }
-                    await sleep(5000);
-                    continue;
-                }
-
-            }
-            
-            results = [...results, ...data.results]
-            if (!data.next) {
-                return new Promise((resolve, reject) => {
-                    resolve(results)
-                });
-            }
-            reqUrl = url + data["next"];
-            itr += 1;
-            attempt = 0;
-        }
-
-    } catch (error) {
-        console.error(error);
+    let params = {
+        "l": "wt-wt",
+        "o": "json",
+        "q": keywords,
+        "vqd": token,
+        "f": ",,,",
+        "p": "" + (p)
     }
-    return results;
 
+    let data = null;
+    let itr = 0;
+
+
+    while (itr < iterations) {
+
+        while (true) {
+            try {
+
+                let response = await axios.get(reqUrl, {
+                    params,
+                    headers
+                })
+
+                data = response.data;
+                if (!data.results) throw "No results";
+                break;
+
+            } catch (error) {
+                console.error(error)
+                attempt += 1;
+                if (attempt > retries) {
+                    return new Promise((resolve, reject) => {
+                        resolve(results)
+                    });
+                }
+                await sleep(5000);
+                continue;
+            }
+
+        }
+            
+        results = [...results, ...data.results]
+        if (!data.next) {
+            return new Promise((resolve, reject) => {
+                resolve(results)
+            });
+        }
+        reqUrl = url + data["next"];
+        itr += 1;
+        attempt = 0;
+    }
+
+    return results;
 }
 
 
